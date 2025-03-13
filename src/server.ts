f4754b9a5fd2065ff7c596c9d5094b0fb732aa81
Musab74 <Rmcbaxr@gmail.com>
2025-03-14 00:33:45 +0900

    feat:connection mongoDb

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,19 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("Port:", process.env.PORT);
+// console.log("Port:", process.env.PORT);
 
-console.log("MONGO_URL:", process.env.MONGO_URL);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
+
+//cluster => Database => collection => Document
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {}).
+then((date) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    
+})
+.catch((err) => console.log("Error on connection MOngoDb", err)
+)
