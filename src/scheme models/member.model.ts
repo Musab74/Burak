d6c81b326a276ae5062c7b8ae2bef5e5c058aba2
Musab74 <Rmcbaxr@gmail.com>
2025-03-15 22:00:16 +0900

    feat: member schema

--- src/scheme models/member.model.ts
+++ src/scheme models/member.model.ts
@@ -23,4 +23,32 @@ const memberSchema = new Schema({
         index: {unique: true, sparse: true},
         required: true,
     },
-})
+
+    memberPhone: {
+        type: String,
+        index: {unique: true, sparse: true},
+        required: true,
+    },
+
+    memberPassword: {
+        type: String,
+        select:false,
+        required: true,
+    },
+    memberAdress: {
+        type:String,
+    },
+    memberDescription: {
+        type: String,
+    },
+    memberImage: {
+        type: String,
+    },
+    memberPoints: {
+        type: Number,
+        default: 0,
+    },
+
+}, {timestamps: true} // UpdatedAd 
+);
+export default mongoose.model("member",memberSchema);
